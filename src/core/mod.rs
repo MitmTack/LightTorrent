41cb7414416753a3d8055a79dd1a349148c147be
MitmTack <nerlesfouns@gmail.com>
2026-04-15 16:39:08 +0000

    Delete src directory

--- src/core/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod engine;
-pub mod torrent;
-
-pub use engine::Eng;
-pub use engine::{Cfg, ldcfg, svcfg, exedir, plugdir};
-pub use torrent::State;
--- src/ui/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod app;
-pub mod theme;
-pub mod widgets;
-pub mod lang;
